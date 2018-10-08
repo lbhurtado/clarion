776429512b17b324fb6bfe54517d288e4059c35c
Lester B. Hurtado <lester@hurtado.ph>
2018-10-08 15:05:43 +0800

    do not apply ucfirst to handle attribute in User model

--- app/Domain/Models/User.php
+++ app/Domain/Models/User.php
@@ -75,8 +75,8 @@ class User extends Authenticatable implements JWTSubject, Transformable
         return $this->verified_at && $this->verified_at->addSeconds(60) <= now();
     }
 
-    public function getHandleAttribute($value)
-    {
-        return ucfirst($value);
-    }
+    // public function getHandleAttribute($value)
+    // {
+    //     return ucfirst($value);
+    // }
 }
